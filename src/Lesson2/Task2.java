9ffaf10bbbf1cc090c9f541600beea110b24468c
FireOfDragon <yaroslav.stupak.2003@gmail.com>
2022-07-31 23:57:48 +0300

    Task2upd

--- src/Lesson2/Task2.java
+++ src/Lesson2/Task2.java
@@ -1,7 +1,14 @@
 package Lesson2;
 
 public class Task2 {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        int a = (int) (Math.random() * 10);
+        int b = (int) (Math.random() * 10);
+        int r = (int) (Math.random() * 10);
+        int rect = a * b;
+        double circle = Math.PI * Math.pow(r,2);
+        System.out.println("Площа прямокутника зі сторонами а = " + a + " та b = " + b + " дорівнює " + rect);
+        System.out.println("Площа кола з радіусом r = " + r + " дорівнює " + circle);
     }
 }
